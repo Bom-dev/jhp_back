eb96f44ad31f7a0060d74209343ee9c5729a6877
Bom <11377ny@gmail.com>
2022-09-05 20:54:00 -0400

    Install heroku app

--- jhp_back/settings.py
+++ jhp_back/settings.py
@@ -68,8 +68,9 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "https://domain.com",
-    "https://localhost:3000"
-    "https://127.0.0.1:3000"
+    "https://localhost:3000",
+    "https://127.0.0.1:3000",
+    "https://job-hunters-point.herokuapp.com/",
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
